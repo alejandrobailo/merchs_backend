18cb78b87aa17c3f20514cf8b682318d9732ab9c
Antxon Eguiguren <antxonegi@gmail.com>
2020-03-17 12:47:19 +0100

    Merge

--- models/size.js
+++ models/size.js
@@ -8,7 +8,6 @@ const getAll = () => {
 const createSizesRelation = (sizesQuantities, productId) => {
     return new Promise((resolve, reject) => {
 
-<<<<<<< HEAD
         sizesQuantities = JSON.parse(sizesQuantities);
 
         for (item of sizesQuantities) {
@@ -18,25 +17,6 @@ const createSizesRelation = (sizesQuantities, productId) => {
                     resolve(result);
                 });
         }
-=======
-const createSizes = (sizesQuantities, productId) => {
-    console.log(sizesQuantities);
-    return new Promise((resolve, reject) => {
-
-        resolve(console.log('Tallas:'));
-        reject(console.log('Tallas:'));
-        console.log(sizesQuantities);
-        // JSON.parse(sizesQuantities);
-        // console.log(sizesQuantities);
-
-        // sizesQuantities.forEach((item) => {
-        //     db.query('insert into tbi_size_product (fk_product, fk_size, quantity) values (?, ?, ?)',
-        //         [productId, item.size, item.quantity], (err, result) => {
-        //             if (err) reject(err);
-        //             resolve(result);
-        //         });
-        // });
->>>>>>> feature_dev_antxon
     });
 }
 

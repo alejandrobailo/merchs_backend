9e75fb12964b55ef2e19198cdd937be1f71b326e
Alejandro <alejandrobailo94@gmail.com>
2020-03-20 11:05:28 +0100

    Añadido dashboard como inicio

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/dashboard', function (req, res, next) {
+  res.render('pages/index', { title: 'Express' });
 });
 
 module.exports = router;

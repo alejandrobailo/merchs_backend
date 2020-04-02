dce69b489c33229168ff5301bc61b7aa8bbaf2db
Antxon Eguiguren <antxonegi@gmail.com>
2020-04-02 18:03:38 +0200

    Invoice funcionando para admin y user con sus zapatos correspondientes

--- routes/orders.js
+++ routes/orders.js
@@ -25,7 +25,12 @@ router.get('/', async (req, res) => {
 
 // GET http://localhost:3000/orders/:id
 router.get('/:id', async (req, res) => {
-    const orders = await Order.getAllOrdersByUser(res.locals.user.id);
+    if (res.locals.user) {
+        var orders = await Order.getAllOrdersByUser(res.locals.user.id);
+    }
+    else {
+        var orders = await Order.getAllOrders();
+    }
     const arrOrdersById = [];
     const sizes = await Size.getAll();
     for (order of orders) {
@@ -42,7 +47,12 @@ router.get('/:id', async (req, res) => {
 
 // GET http://localhost:3000/orders/:id/invoice
 router.get('/:id/invoice', async (req, res) => {
-    const orders = await Order.getAllOrders();
+    if (res.locals.user) {
+        var orders = await Order.getAllOrdersByUser(res.locals.user.id);
+    }
+    else {
+        var orders = await Order.getAllOrders();
+    }
     const arrOrdersById = [];
     for (order of orders) {
         if (order.fk_order === parseInt(req.params.id)) {
